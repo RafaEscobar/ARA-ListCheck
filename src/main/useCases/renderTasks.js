23b825a58bad128a1bda8e2ab95080e3467776fd
RafaEscobar <Rafael.EG.code@gmail.com>
2023-09-17 13:30:08 -0600

    Agregando btn de eliminación de tarea

--- src/main/useCases/renderTasks.js
+++ src/main/useCases/renderTasks.js
@@ -11,16 +11,23 @@ let content;
 const generateContentTask = (task) => {
     const contentTask = document.createElement('div');
     contentTask.innerHTML = `
-        <div class="checkbox-wrapper-15 my-4">
-            <input class="inp-cbx" id="${task.id}" type="checkbox" style="display: none;" />
-            <label class="cbx" for="${task.id}">
-              <span class="inline-block">
-                <svg viewbox="0 0 12 9" class="checkIcon">
-                  <polyline points="1 5 4 8 11 1"></polyline>
-                </svg>
-              </span>
-              <span class="font-mono text-lg md:text-xl">${task.description}</span>
-            </label>
+        <div class="checkbox-wrapper-15 my-4 flex justify-between items-center">
+            <div>
+                <input class="inp-cbx" id="${task.id}" type="checkbox" style="display: none;" />
+                <label class="cbx" for="${task.id}">
+                  <span class="inline-block">
+                    <svg viewbox="0 0 12 9" class="checkIcon">
+                      <polyline points="1 5 4 8 11 1"></polyline>
+                    </svg>
+                  </span>
+                  <span class="font-mono text-lg md:text-xl">${task.description}</span>
+                </label>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 md:w-8 md:h-8" id="deleteBtn">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>          
+            </div>
         </div>`
     contentTask.setAttribute('data-id', task.id);
 
