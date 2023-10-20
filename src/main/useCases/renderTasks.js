1920976d2bf61bd3a7188f802bc5aa2f8ff906c5
RafaEscobar <Rafael.EG.code@gmail.com>
2023-10-19 21:43:56 -0600

    BreackPoint

--- src/main/useCases/renderTasks.js
+++ src/main/useCases/renderTasks.js
@@ -26,7 +26,6 @@ const generateContentTask = (task) => {
             <div class="flex justify-between gap-4">
               <span class="font-mono text-lg md:text-xl">${ (task.time != null) ? task.time : '' }</span>
               <div class="cursor-pointer transform hover:scale-125 transition duration-600 w-6 h-6 md:w-8 md:h-8" >
-                  
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 md:w-8 md:h-8" id="deleteBtn">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg>          
