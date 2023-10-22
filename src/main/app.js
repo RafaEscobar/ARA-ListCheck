2518afa52882790af50af2633a99b04741cd7cd9
RafaEscobar <Rafael.EG.code@gmail.com>
2023-10-22 14:41:03 -0600

    Agregando eventos para btns de filtros

--- src/main/app.js
+++ src/main/app.js
@@ -16,6 +16,9 @@ export const idCollection = {
     inputTimePiker: '#inputTimePiker',
     btnTimeError: '#btnTimeError',
     textTimeError: '#textTimeError',
+    allFilter: '#allFilter',
+    pendingFilter: '#pendingFilter',
+    completedFIlter: '#completedFilter',
 };
 
 /**
@@ -44,6 +47,9 @@ export const app = () => {
     let inputTimePiker = document.querySelector(idCollection.inputTimePiker);
     let btnTimeError = document.querySelector(idCollection.btnTimeError);
     let textTimeError = document.querySelector(idCollection.textTimeError);
+    let filterAll = document.querySelector(idCollection.allFilter);
+    let filterPending = document.querySelector(idCollection.pendingFilter);
+    let filterCompleted = document.querySelector(idCollection.completedFIlter);
 
     //!Assigments
     inputDescriptionTask.value = null;
@@ -70,12 +76,14 @@ export const app = () => {
         btnClock.setAttribute('disabled', 'true');
     }
 
+    //! Event Listener
+
+    //* Click event for btnClock - Put btnSaveTime as disabled and inputTimePiker as ''
     btnClock.addEventListener('click', () => {
         btnSaveTime.setAttribute('disabled', true);
         inputTimePiker.value = '';
     });
 
-    //! Event Listener
     //* Keydown event for inputTask - Create a new task in state.tasks
     inputTask.addEventListener('keyup', (event) => {
         if ( inputDescriptionTask.value.trim().length == 0 ) return;
@@ -109,7 +117,7 @@ export const app = () => {
         }
     });
 
-    //* Entry event for the modal timePicker
+    //* Input event for the modal inputTimePicker
     inputTimePiker.addEventListener('input', () => {
         if ( inputTimePiker.value == '' ) return;
         let messageError = 'La hora proporcionada ya paso... elije una hora válida.';
@@ -144,6 +152,21 @@ export const app = () => {
         if ( inputTimePiker.value == '' || inputDescriptionTask.value == null ) return;
         flowToCreateTask(moment(`${inputTimePiker.value}`, "hh:mm").format('hh:mm a'));
         inputTimePiker.value = null;
+    });
+
+    //* Click event for filter all tasks
+    filterAll.addEventListener('click', () => {
+        
+    });
+
+    //* Click event for filter pending tasks
+    filterPending.addEventListener('click', () => {
+
+    });
+
+    //* Click event for filter completed tasks
+    filterCompleted.addEventListener('click', () => {
+
     });
 
     setInterval(() => {
